d5482867532092be1c2d8a6c94d8c6d141a3b01a
李孟 <meng.li@fitow.com>
2022-10-10 14:24:50 +0800

    调整模型大小

--- src/hooks/useThree.ts
+++ src/hooks/useThree.ts
@@ -154,7 +154,7 @@ export function useThree(afterLandingComplete: () => void) {
       console.log(err);
     }
   ); */
-  glbLoader.load("/glbs/standing1.glb", afterLoadHuman, undefined, (err) => {
+  glbLoader.load("/glbs/standing-prcesd.glb.glb", afterLoadHuman, undefined, (err) => {
     moduleLoading.value = false;
   });
 
